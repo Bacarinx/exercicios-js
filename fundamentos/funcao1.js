1a3d5cb5887e5c2ccb377803dffbf6a6ed643e91
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-06 13:49:19 -0300

    create function

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,12 @@
+//função sem retorno
+function imprimirSoma(a, b) {
+    console.log (a + b)
+}
+
+imprimirSoma(2, 4)
+
+//função com retorno
+function soma (a, b= 0) {
+    return a + b
+}
+console.log (soma(2, 3))
