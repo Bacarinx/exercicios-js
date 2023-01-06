a3b7c6ae48bdc47212fc1019957cb6eb51229de1
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-06 09:50:17 -0300

    Create objeto.js

---
+++ fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}
+prod1.nome = "celular Sansung"
+prod1.preco = 2999,90
+prod1.qualidade = "Otima"
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 59.90,
+    qualidade: 'boa'
+}
+console.log (prod2)
