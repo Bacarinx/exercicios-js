3208333696890d1553ba962eb128c1c7022a51f6
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-06 08:35:31 -0300

    Create strings.js

---
+++ fundamentos/strings.js
@@ -0,0 +1,11 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt (4))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log ("escola " + escola + "!")
+
+console.log("Ana,Maria,Andre".split(','))
