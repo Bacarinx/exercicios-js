fe3dad1d123a2193515b1c9caf21aa52e1661d09
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-06 09:11:33 -0300

    create bool

---
+++ fundamentos/templateString.js
@@ -0,0 +1,11 @@
+const nome = 'Rebeca'
+
+const concatenacao = 'Olá ' + nome + '!' //jeito antigo e ruim de concatenar
+const template = `Olá ${nome}!` //jeito atual, usando Crase " ` "
+console.log(concatenacao, template)
+
+//expressões 
+
+console.log(`1 + 1 = ${1 + 1}`)
+
+
