296e19d38952a91be69d26ab2e4af9c2427174b1
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-07 20:51:22 -0300

    Create operadoresUnarios.js

---
+++ fundamentos/operadoresUnarios.js
@@ -0,0 +1,18 @@
+let num1 = 1
+let num2 = 2
+let expoente = 0
+
+num1++ //incremento de 1
+console.log(num1)
+
+num1-- //decremento de 1
+console.log(num1)
+
+expoente = 2 ** 3 //operadores de expoente --> 2 elevado a 3
+console.log(expoente)
+
+expoente **= 2 // --> expoente = expoente ** 2
+console.log (expoente)
+
+expoente **= -1 //--> expoente negativo 
+console.log (expoente)
