8c0ac8560abba5eb46ba5bdbf7010fd6cef1d241
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-12 15:22:40 -0300

    Update heranca03.js

--- objeto/heranca03.js
+++ objeto/heranca03.js
@@ -5,5 +5,17 @@ filha1.nome = "Ana"
 console.log(filha1.corCabelo)
 
 const filha2 = Object.create(pai, {
-    nome: {}
+    nome: {value: 'Bia', writable: false, enumerable: true }
 })
+
+console.log(filha2.nome)
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for (let key in filha2) {
+    if (filha2.hasOwnProperty(key)) {
+        console.log(key) 
+    } else { console.log(`por herança: ${key}`)}
+}
