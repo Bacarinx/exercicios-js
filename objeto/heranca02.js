f806ab534fdf7296b76779f20b1b2f294f461c98
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-12 14:59:20 -0300

    create herança3

--- objeto/heranca02.js
+++ objeto/heranca02.js
@@ -1,12 +1,7 @@
 // cadeia de protótipos (prototype chain)
 const avo = {attr1: 'A'}
-
-const pai = {
-__proto__: avo, attr2: 'B', attr3: '3'}
-
-const filho = {__proto__: pai, attr3: 'C'
-}
-
+const pai = {__proto__: avo, attr2: 'B', attr3: '3'}
+const filho = {__proto__: pai, attr3: 'C'}
 console.log(filho.attr1, filho.attr2, filho.attr3) 
 
 //---------------------------------------------------//
