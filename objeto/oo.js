24f3d12f416a469e72e7b16913e2d704eac8fb86
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-12 08:48:37 -0300

    Create oo.js

---
+++ objeto/oo.js
@@ -0,0 +1,25 @@
+// CÓDIGO NÃO EXECUTAVEL
+
+//PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+//OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() //Foco passou a ser o Objeto
+
+/* Principios importantes:
+
+1. Abstração 
+2. encapsulamento    
+3. herança (prototype)
+4. polimorfismo (multiplas formas)
+
+*/
