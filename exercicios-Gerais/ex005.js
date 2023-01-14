3267562260d368d56a4e369aec73780ae6aff02d
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-14 19:50:43 -0300

    Create ex005.js

---
+++ exercicios-Gerais/ex005.js
@@ -0,0 +1,10 @@
+function maiorOuigual(n1, n2) {
+    if(n1 >= n2) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+}
+maiorOuigual(1, 2)
+maiorOuigual (1, 1)
+maiorOuigual (2, 1)
