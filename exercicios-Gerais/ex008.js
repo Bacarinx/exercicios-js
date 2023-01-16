42923aa605a1f63d067624b93585316f0f61867f
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-15 22:07:10 -0300

    Create ex008.js

---
+++ exercicios-Gerais/ex008.js
@@ -0,0 +1,12 @@
+function multiplicar (num1, num2) {
+    var soma = 0
+    var num = 0
+    while ( num < num1) {
+        soma += num2
+        num++
+    }
+    console.log(soma)
+}
+multiplicar (2, 8)
+
+
