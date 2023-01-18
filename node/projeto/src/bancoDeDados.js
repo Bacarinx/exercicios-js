105029739365c9396a3191b98aa016514fd7373a
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-18 16:04:16 -0300

    2° part projeto

---
+++ node/projeto/src/bancoDeDados.js
@@ -0,0 +1,21 @@
+const sequence = {
+    _id: 1,
+    get id() {return this._id++}
+}
+
+const produtos = {}
+function salvarProduto(produto) {
+    if (!produto.id) produto.id = sequence.id //caso o produto não tenha id, adiciona o id da sequencia
+    produtos[produto.id] = produto //após o produto ter id, recebe o valor do produto
+    return produto
+}
+
+function getProduto(id) {
+   return produtos[id] || {} //adiciona o produto a partir do seu id
+}
+
+function getProdutos() {
+    return Object.values(produtos) //exibe todos os produtos contidos na lista de objetos "produtos"
+}
+
+module.exports = { salvarProduto, getProduto, getProdutos }
--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,22 @@ const porta = 3003
 
 const express = require('express') //chamando o express
 const app = express() //adicionando o express na const app
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 123.99}) // O metodo Send vai converter o Objeto para JSON
+    res.send(bancoDeDados.getProdutos()) // O metodo Send vai converter o Objeto para JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () => {
