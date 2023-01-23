8997efc0cb597c9aa9ba73857aca8259e852db10
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-01-23 15:48:57 -0300

    Update async_await01.js

--- esnext/async_await01.js
+++ esnext/async_await01.js
@@ -1,4 +1,4 @@
-function esperarPor(tempo = 2000) { //função pra criar um tempo de espera
+function esperarPor(tempo) { //função pra criar um tempo de espera
     return new Promise(resolve => {
         setTimeout ( () => {
             resolve(10)
@@ -7,7 +7,7 @@ function esperarPor(tempo = 2000) { //função pra criar um tempo de espera
 }
 
 async function executar() {
-    let valor = await esperarPor()
+    let valor = await esperarPor() //variavel 'Valor' recebe o valor do resolve da promise de cima 
 
     await esperarPor(1000)
     console.log(`Async/Await ${valor}`)
