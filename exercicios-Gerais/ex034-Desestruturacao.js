51f49103de6b3364d18782da80a0fe3f27f9ea03
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-22 15:00:29 -0300

    rest Operator

--- exercicios-Gerais/ex034-Desestruturacao.js
+++ exercicios-Gerais/ex034-Desestruturacao.js
@@ -35,20 +35,7 @@ function mostraIdade({idade, nickname = "baca"}) {
 document.body.innerText = mostraIdade(user)
 
 //-------------------------------------------------------------------//
-// Rest operator 
 
-// quando eu utilizo o operador rest ..., o valor retornado será o restando do objeto/array que está no "resto" dele, ou seja, toda a informação restante, tirando o "name" neste caso
-
-const { name, ...rest } = user; //desestruturando e rest
-
-const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]; //array padrão
-
-const ultimo = numbers[9] //forma padrão 
-
-const [first, second, ...resto] = numbers;
-
-
-document.body.innerText = JSON.stringify({first, second, resto})
 
 
 
